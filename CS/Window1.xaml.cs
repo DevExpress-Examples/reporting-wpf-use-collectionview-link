8f767e2f579682b407ba04c438da18400a5c3492
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Window1.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using DevExpress.Xpf.Printing;
+
+namespace UseCollectionViewLink {
+
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+
+        #region #CollectionViewLink
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            // Create a link and bind it to the PrintPreview instance.
+            CollectionViewLink link = new CollectionViewLink();
+            preview.Model = new LinkPreviewModel(link);
+
+            // Create an ICollectionView object.
+            link.CollectionView = CreateMonthCollectionView();
+
+            // Provide export templates.
+            link.Detail = (DataTemplate)Resources["monthNameTemplate"];
+            link.GroupInfos.Add(new GroupInfo((DataTemplate)Resources["monthQuarterTemplate"]));
+
+            // Create a document.
+            link.CreateDocument(true);
+        }
+
+
+        private ICollectionView CreateMonthCollectionView() {
+            const int monthCount = 12;
+            string[] monthNames = CultureInfo.CurrentCulture.DateTimeFormat.MonthNames;
+
+            MonthItem[] data = new MonthItem[Math.Min(monthNames.Length, monthCount)];
+            for (int i = 0; i < data.Length; i++) {
+                data[i] = new MonthItem(monthNames[i], (i / 3) + 1);
+            }
+
+            CollectionViewSource source = new CollectionViewSource();
+            source.Source = data;
+            source.GroupDescriptions.Add(new PropertyGroupDescription("Quarter"));
+
+            return source.View;
+        }
+
+        public class MonthItem {
+            public string Name { get; private set; }
+            public int Quarter { get; private set; }
+
+            public MonthItem(string name, int quarter) {
+                Name = name;
+                Quarter = quarter;
+            }
+        }
+        #endregion #CollectionViewLink
+
+    }
+
+}
---
+++ Readme.md
@@ -0,0 +1,14 @@
+# How to use the CollectionView Link
+
+
+<p>The following example demonstrates how to use the <strong>CollectionViewLink</strong> class to print data from a hierarchical data source, which implements the <strong>ICollectionView</strong> interface.</p><p>See also: <a href="https://www.devexpress.com/Support/Center/p/E1673">How to use the Simple Link</a></p>
+
+
+<h3>Description</h3>
+
+<p><br />
+</p>
+
+<br/>
+
+
