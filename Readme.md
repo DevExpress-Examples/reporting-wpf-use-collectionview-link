c72f9893737a862b665c6d61793171f781f9e130
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to use the CollectionView Link
+
+
+<p>The following example demonstrates how to use the <strong>CollectionViewLink</strong> class to print data from a hierarchical data source, which implements the <strong>ICollectionView</strong> interface.</p><p>See also: <a href="https://www.devexpress.com/Support/Center/p/E1673">How to use the Simple Link</a></p>
+
+<br/>
+
+
---
+++ VB/Window1.xaml.vb
@@ -0,0 +1,80 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.ComponentModel
+Imports System.Globalization
+Imports System.Windows
+Imports System.Windows.Data
+Imports DevExpress.Xpf.Printing
+
+Namespace UseCollectionViewLink
+
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		#Region "#CollectionViewLink"
+		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			' Create a link and bind it to the PrintPreview instance.
+			Dim link As New CollectionViewLink()
+			preview.Model = New LinkPreviewModel(link)
+
+			' Create an ICollectionView object.
+			link.CollectionView = CreateMonthCollectionView()
+
+			' Provide export templates.
+			link.DetailTemplate = CType(Resources("monthNameTemplate"), DataTemplate)
+			link.GroupInfos.Add(New GroupInfo(CType(Resources("monthQuarterTemplate"), DataTemplate)))
+
+			' Create a document.
+			link.CreateDocument(True)
+		End Sub
+
+
+		Private Function CreateMonthCollectionView() As ICollectionView
+			Const monthCount As Integer = 12
+			Dim monthNames() As String = CultureInfo.CurrentCulture.DateTimeFormat.MonthNames
+
+			Dim data(Math.Min(monthNames.Length, monthCount) - 1) As MonthItem
+			For i As Integer = 0 To data.Length - 1
+				data(i) = New MonthItem(monthNames(i), (i \ 3) + 1)
+			Next i
+
+			Dim source As New CollectionViewSource()
+			source.Source = data
+			source.GroupDescriptions.Add(New PropertyGroupDescription("Quarter"))
+
+			Return source.View
+		End Function
+
+		Public Class MonthItem
+			Private privateName As String
+			Public Property Name() As String
+				Get
+					Return privateName
+				End Get
+				Private Set(ByVal value As String)
+					privateName = value
+				End Set
+			End Property
+			Private privateQuarter As Integer
+			Public Property Quarter() As Integer
+				Get
+					Return privateQuarter
+				End Get
+				Private Set(ByVal value As Integer)
+					privateQuarter = value
+				End Set
+			End Property
+
+			Public Sub New(ByVal name As String, ByVal quarter As Integer)
+				Name = name
+				Quarter = quarter
+			End Sub
+		End Class
+		#End Region ' #CollectionViewLink
+
+	End Class
+
+End Namespace
